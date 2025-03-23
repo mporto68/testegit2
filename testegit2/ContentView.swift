0a0d6494a674ab8bc2672fea9573c39a846b97cd
Marcus Porto <mporto@sarrat.com.br>
2025-03-22 21:32:56 -0300

    Primeiro commit com o git remote configurado

--- testegit2/ContentView.swift
+++ testegit2/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "marcus")
+            Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Marcus")
         }
         .padding()
     }
